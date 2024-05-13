e741fdbb3ca60b59bd6b8eb3d6eb0160dd18afd4
matyo-17 <filsonteo@gmail.com>
2024-05-14 00:09:31 +0800

    get all leagues

--- src/Fotmob.php
+++ src/Fotmob.php
@@ -2,7 +2,51 @@
 
 namespace Matyo17\FotmobSdk;
 
-Class Fotmob {
+use Httpful\Request;
+use Httpful\Response;
 
+Class Fotmob {
+    private static string $api_url = "https://www.fotmob.com/api/";
+
+    public function __construct() {
+        
+    }
+
+    public function get_all_leagues() {
+        return $this->get(self::$api_url."allLeagues");
+    }
+
+    protected function get(string $url, $payload = null) {
+        return $this->call_api('get', $url, $payload);
+    }
+
+    private function call_api(string $method, string $url, $payload = null) {
+        $method = strtolower($method);
+
+        switch ($method) {
+            case 'post':
+                $request = Request::post($url, $payload);
+                break;
+            case 'patch':
+                $request = Request::patch($url, $payload);
+                break;
+            case 'delete':
+                $request = Request::delete($url);
+                break;
+            case 'put':
+                $request = Request::put($url);
+                break;
+            default:
+                $request = Request::get($url);
+                break;
+        }
+
+        return $this->map_response($request->send());
+    }
+
+    private function map_response(Response $response) {
+        $body = $response->body;
+        return json_decode(json_encode($body), true);
+    }
 }
 ?>
--- tests/test.php
+++ tests/test.php
@@ -4,4 +4,6 @@ require __DIR__.'/../vendor/autoload.php';
 
 use Matyo17\FotmobSdk\Fotmob;
 
+$fm = new Fotmob();
+$fm->get_all_leagues();
 ?>
